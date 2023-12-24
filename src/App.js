5ada8905384f4a12a62b52e6c1fe8a42457a154e
Hieuhd26 <trunghieutranst@gmail.com>
2023-12-24 23:38:27 +0700

    push code to github

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">  
+    <User1/>
+    </div>
+  );
+}
+const User1 = () => {
+  return (
+    <>
+     <h1>Ha</h1>
+     <h1>22</h1>
+     <h1>hailua@gmail.com</h1>
+    </>
+  );
+}
+
+
+export default App;
