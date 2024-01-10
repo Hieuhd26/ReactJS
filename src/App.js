e4f905d09626913e3611f31fe169980d6869ad54
Hieuhd26 <trunghieutranst@gmail.com>
2024-01-10 10:22:57 +0700

    Code W1

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
-import './App.css';
+import styles from "./App.module.css";
+import { useState } from "react";
 
 function App() {
   return (
-    <div className="App">  
-    <User1/>
+    <div className="App">
+      <Job salary={2000} position="Back-end" company="Amazon" />
+      <Job salary={3000} position="Front-end" company="Tik Tok" />
+      <Job salary={4000} position="DevOpts" company="Youtube" />
     </div>
   );
 }
-const User1 = () => {
-  return (
-    <>
-     <h1>Ha</h1>
-     <h1>22</h1>
-     <h1>hailua@gmail.com</h1>
-    </>
-  );
-}
 
+const Job = (props) => {
+  return (
+    <div>
+      <h1><i>{props.salary}</i></h1>
+      <h1><i>{props.position}</i></h1>
+      <h1><i>{props.company}</i></h1>
+    </div>
+  );
+};
 
 export default App;
